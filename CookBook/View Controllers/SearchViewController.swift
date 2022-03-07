6050d64821f4b92608a234455cb55b541c5f092f
Komal Kaur <44416323+Komal914@users.noreply.github.com>
2022-03-07 01:09:33 -0500

    added lottie as a package for the project

--- CookBook/View Controllers/SearchViewController.swift
+++ CookBook/View Controllers/SearchViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreData
+import Lottie
 class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var managedObjectContext: NSManagedObjectContext!
